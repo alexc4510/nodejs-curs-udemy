70c0922cd879a7ec7fb1f80d22da2792059ad5d5
alex4510 <alex.craciun2003@gmail.com>
2024-02-03 15:37:07 +0200

    ma joc

--- play.js
+++ play.js
@@ -1,3 +1,38 @@
-var nume='alex'
+const cl=console.log;
+const person={
+    name:'Alex',
+    age:29,
+    greet() {
+        cl('Hi, I am ' + this.name)
+    }
+};
 
-console.log(nume)
+const printName = ({ name }) => {
+    cl(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+cl(name, age);
+
+
+const hobbies=['Sports','Cooking'];
+const [hobby1, hobby2]=hobbies;
+cl(hobby1, hobby2);
+
+// //for (let hobby of hobbies) {
+// //    cl(hobby);
+// //}
+
+// copiedArray=[...hobbies]
+// cl(copiedArray)
+
+// copiedPerson={...person}
+// cl(copiedPerson)
+
+// const toArray = (...args) => {
+//     return args;
+// };
+
+// cl(toArray(1,2,3,4));
