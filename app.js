85e35d3ab520630800d413ec0d8384c028d823aa
alex4510 <alex.craciun2003@gmail.com>
2024-02-24 17:26:23 +0200

    hehe progres

---
+++ app.js
@@ -0,0 +1,25 @@
+const cl = console.log;
+const express = require('express');
+const bodyParser = require ('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><button type = "submit">Add product</button></form>');
+});
+
+app.post('/product',(req, res, next) => {
+    cl(req.body);
+    res.redirect('/');
+
+});
+
+app.use('/', (req, res, next) => {
+
+    res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(3003);
